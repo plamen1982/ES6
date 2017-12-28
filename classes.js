797adc90811b70698f1208706389c16448c39b27
“[plamen1982]” <plamen.hristov@ymail.com>
2017-12-28 12:21:39 -0500

    forOfLoop and generators

--- classes.js
+++ classes.js
@@ -39,7 +39,7 @@ class CarES6 {
 
 class ToyotaES6 extends CarES6 {
     constructor(options) {
-        //super calls parent constructor
+        //super calls parent constructor -> Car.constructor()
         super(options);
         this.color = options.color;
     }
@@ -54,3 +54,50 @@ console.log(toyotaES6);
 console.log(toyotaES6.drive());
 console.log(toyotaES6.honk());
 
+/**Ex1: Game Classes
+ * You are a game developer tasked with setting up some basic classes for a new game you are working on.  
+ * Create a class called 'Monster'.  In the constructor, you'll need to do some basic setup for Monster 
+ * whenever they are created. 
+ * Initialize the Monster's health to 100.The constructor will be called with an 'options' object that 
+ * has a 'name' property.  Assign the 'name' to the Monster
+ */
+
+class Monster {
+    constructor(options) {
+        this.name = options.name;
+        this.health = 100;
+    }
+    
+}
+
+const monster = new Monster({ name: 'The red monster' })
+console.log(`The monster's health is ${monster.health} and the name of the monster is ${monster.name}`)
+
+
+/**Ex2: Subclassing Monsters
+ * Now that you have a monster created, create a subclass of the Monster called Snake. 
+ * The Snake should have a 'bite' method.  The only argument to this method is another 
+ * instance of a Snake. The instance of Snake that is passed in should have their health
+ * deducated by 10
+ */
+
+  
+  class Snake extends Monster {
+      
+      constructor(options) {
+          super(options)
+      }
+      
+      bite(instance) {
+          
+          return instance.health = instance.health - 10        
+      }
+  
+  }
+  
+  const snake = new Snake({ name: 'Snake 1' })
+  const snake2 = new Snake({ name: 'Snake 2' })
+  snake2.bite(snake)
+  snake2.bite(snake)  
+
+  console.log(snake.health) // snake.health = 80, after two bites from snake2
---
+++ generators.js
@@ -0,0 +1,108 @@
+//1. What is a generator - is a function that can be entered and exit multiple times. Normally function can
+//run once and that's it. With generator we can run some code, return a value and then return right back
+//to the function at the same place that we left it. To define a function to be a generator place 
+//an asterisk after the key word function and inside the function we need to have the key word yield
+
+
+function* numbers() {
+    yield;
+}
+
+const gen = numbers();
+
+//2. What does a generator do?
+
+function* shopping() {
+    //stuff on the sidewalk
+
+    //walking down the sidewalk
+
+    //go into the store with cash
+
+    const stuffFromStore = yield 'cash'; //transition (yield) the value 'cash' to first gen2.next()
+
+    const cleanClothes = yield 'laundry';
+    //welcome back home
+    return [stuffFromStore, cleanClothes]
+}
+
+//stuff in the store
+const gen2 = shopping();// nothing happeneds inside our generator, yet
+
+console.log(gen2.next()); //leave out house - executing the shopping generator function for the 
+//first time, { value: 'cash', done: false}
+
+
+console.log(gen2.next('groceries')); //return to first yield and continue to second yield, 
+//{ value: 'laudry', done: false }
+
+console.log(gen2.next('clean clothes')); //leave the store with groceries - return back to the secund yield
+//with and return  { value: ['groceries', done: true }
+
+
+//3. Iteration with generators
+
+function* colors() {
+    yield 'white';
+    yield 'green';
+    yield 'red';
+}
+
+// without iterations
+const genColors = colors();
+console.log(genColors.next()); // { value: 'white', done: false }
+console.log(genColors.next()); // { value: 'green', done: false }
+console.log(genColors.next()); // { value: 'red', done: false }
+console.log(genColors.next()); // { value: udefined, done: true }
+
+//with iterations
+let myColors = [];
+for (let color of colors()) {
+    //we do not need .next() with this approach
+
+    myColors.push(color);
+}
+console.log(myColors)
+
+//4. Delegation of generator
+//Practical use case
+const engineeringTeam = { 
+    testingTeam,    //reference to the testingTeam object -> testingTeam: testingTeam
+    size: 3, 
+    department: 'Engineering',
+    lead: 'Jil',
+    manager: 'Alex',
+    engineer: 'Dave',
+
+}
+
+const testingTeam = {
+    lead: 'Amanda',
+    tester: 'Bill'
+}
+
+function* TeamIterator(team) {
+    yield team.lead;
+    yield team.manager;
+    yield team.engineer;
+    const testingTeamGenerator = TestingTeamIterator(team.testingTeam); //creating generator
+    yield* testingTeamGenerator; //yield* is used when we have more than one yield in the generator 
+//(in this case testingTeamGenerator)
+}
+
+function* TestingTeamIterator(team) {
+    yield team.lead;
+    yield team.tester;
+}
+
+const names = [];
+for (let name of TeamIterator(engineeringTeam)) {
+    names.push(name)
+}
+console.log(names)
+
+//Engineering Team -> Lead, Manager, Engineer; Testing Team -> Lead, Tester
+
+
+
+//5. Generators with Symbol.iterator
